d36c82ec71707a0b5a26730d0831dacf202192f3
Shashank Dhakate <shashankdkte@gmail.com>
2023-11-27 21:04:00 +0530

    Adding First Mini Project

---
+++ src/components/birthday-remainder/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import BirthdayRemainder from './BirthdayRemainder';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BirthdayRemainder />
+  </React.StrictMode>
+);
